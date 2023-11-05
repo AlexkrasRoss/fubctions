33f2094725ce6122b673e5716090739c6dc98ee0
Alex hitsaaja <sto98@inbox.ru>
2023-11-05 13:05:54 +0300

    Cheange readme

--- task.js
+++ task.js
@@ -1,9 +1,21 @@
 function getArrayParams(...arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++)
+   sum += arr[i];
+   let avg =(arr.length ? sum / arr.length : 0);
+  avg = Number(avg.toFixed(2));
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  return ({min: min, max: max,avg: avg});
+}
+getArrayParams(154,152,800);
+   
 
   return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
+
 
 }
 
@@ -16,8 +28,11 @@ function differenceEvenOddWorker(...arr) {
 }
 
 function averageEvenElementsWorker(...arr) {
-
+ 
 }
+summElementsWorker(1,3,4,5,37);
+
+
 
 function makeWork (arrOfArr, func) {
 
